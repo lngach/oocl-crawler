86c185cb733ae747831c8daa70c8e82d65109ba8
lngach <nam.le@gumiviet.com>
2020-08-28 19:04:36 +0700

    Fix missing semi on symbol

--- app/controllers/oocl_controller.rb
+++ app/controllers/oocl_controller.rb
@@ -4,7 +4,7 @@ class OoclController < ApplicationController
     if containers.present?
       crawler = Crawler::OoclService.new(containers)
       crawled = crawler.execute
-      if crawled[:success] && !crawled[containers].empty?
+      if crawled[:success] && !crawled[:containers].empty?
         json_response(crawled, :ok)
       elsif crawled[:success] && crawled[:containers].empty?
         json_response({ success: crawled[:success], message: "Containers not found #{containers.join(', ')}" }, :not_found)
